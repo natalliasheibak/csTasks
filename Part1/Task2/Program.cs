58e3bb6253184fd4242f203e3741a5c2891cce20
Natallia_Sheibak <Natallia_Sheibak@epam.com>
2016-04-21 10:36:18 +0300

    Renamed projects

---
+++ Part1/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+
+namespace FirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b=0;
+            String readConf = System.Configuration.ConfigurationManager.AppSettings["read"];
+            if (readConf.Equals("console"))
+            {
+                Console.WriteLine("Input a");
+                bool aIsNumber = Int32.TryParse(Console.ReadLine(), out a);
+                Console.WriteLine("Input b");
+                bool bIsNumber = Int32.TryParse(Console.ReadLine(), out b);
+                if (!aIsNumber || !bIsNumber)
+                {
+                    Console.WriteLine("Incorrect input");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            else if (readConf.Equals("resourceFile"))
+            {                
+                a=Int32.Parse(Resource1.a);
+                b=Int32.Parse(Resource1.b);
+            }
+
+            String placeConf = System.Configuration.ConfigurationManager.AppSettings["choice"];
+            if (placeConf.Equals("library"))
+            {
+                Calculator cal = new Calculator();
+                Console.WriteLine("a+b=" + cal.plus(a, b));
+                Console.WriteLine("a-b=" + cal.minus(a, b));
+                Console.WriteLine("a*b=" + cal.multiply(a, b));
+                Console.WriteLine("a/b=" + cal.divide(a, b));
+            }
+            else if (placeConf.Equals("method"))
+            {
+                Console.WriteLine(a + b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Part2/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a path to the file you want to read");
+            string path=Console.ReadLine();
+            string text = File.ReadAllText(path);
+            text = text.ToLower();
+            text = String.Format("{0: dd.MM.yyyy HH:mm:ss.fff} ", DateTime.Now)+text.Replace(".", String.Format(".\n{0: dd.MM.yyyy HH:mm:ss.fff}", DateTime.Now));
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
