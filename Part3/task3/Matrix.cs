ca6a21b0100da665a9c1eecb91b3d196c3d748aa
Natallia_Sheibak <Natallia_Sheibak@epam.com>
2016-04-21 15:53:34 +0300

    Updated Part3

--- Part3/task3/Matrix.cs
+++ Part3/task3/Matrix.cs
@@ -3,10 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
 
 namespace task3
 {
-    class Matrix
+    public class Matrix
     {
+        public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+        {
+            try
+            {
+
+
+                if (matrix1.GetLength(1)<1|| matrix2.GetLength(1) < 1 ||matrix1.GetLength(1)!=matrix2.GetLength(0))
+                {
+                    throw new ArithmeticException("Matrices cannot be multuplyed");
+                }
+                else
+                {
+                    double[,] multyMatrix = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+                    for(int i = 0; i < matrix1.GetLength(0); i++)
+                    {
+                        for(int j = 0; j < matrix2.GetLength(1); j++)
+                        {
+                            double summ = 0;
+                            for(int k = 0; k < matrix1.GetLength(1);k++)
+                            {
+                                summ += matrix1[i, k] * matrix2[k, j];
+                            }
+                            multyMatrix[i, j] = summ;
+                        }
+                    }
+                    return multyMatrix;
+                }
+            }
+            catch(FormatException e)
+            {
+                throw new FormatException("Incorrect matrix");
+            }
+        }
+
+        public static double[,] ReadMatrixFromFile(string path)
+        {
+            double[,] matrix = null;
+            string[] buf = File.ReadAllLines(path);
+            if (buf.Length != 0)
+            {
+                matrix = new double[buf.Length, buf[0].Split(' ').Length];
+                try
+                {
+                    int i = 0;
+                    foreach (string line in buf)
+                    {
+                        double[] row = line.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(n => double.Parse(n)).ToArray();
+                        int j = 0;
+                        foreach(double n in row)
+                        {
+                            matrix[i, j] = n;
+                            j++;
+                        }
+                        i++;
+                    }
+                }
+                catch(FormatException e)
+                {
+                    throw new FormatException("Incorrect matrix: format of the number is not correct");
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    throw new IndexOutOfRangeException("Incorrect matrix");
+                }
+            }
+            else
+            {
+                return new double[0,0];
+            }
+            
+            return matrix;
+        }
     }
 }
---
+++ Part3/task4console/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using task3;
+
+namespace task4console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string matrix1Path = ConfigurationManager.AppSettings["Matrix1"];
+            string matrix2Path = ConfigurationManager.AppSettings["Matrix2"];
+            try
+            {
+                double[,] matrix1 = Matrix.ReadMatrixFromFile(matrix1Path);
+                double[,] matrix2 = Matrix.ReadMatrixFromFile(matrix2Path);
+            
+                double[,] multiMatrix = Matrix.Multiply(matrix1, matrix2);
+
+                for (int i = 0; i < multiMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < multiMatrix.GetLength(1); j++)
+                    {
+                        Console.Write(multiMatrix[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch(ArithmeticException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
