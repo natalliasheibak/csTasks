e160df6052a10de8379287922128f1e5cd10355c
Natallia_Sheibak <Natallia_Sheibak@epam.com>
2016-05-05 14:42:42 +0300

    Updated part 5

---
+++ Part5/ClassLibrary1/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace task3
+{
+    class Calculator
+    {
+        public static BigInteger Fibonacci(int number)
+        {
+            BigInteger fibonacci = 1;
+            BigInteger firstNum = 1;
+            BigInteger secondNum = 1;
+            if (number == 1 || number == 2)
+            {
+                return 1;
+            }
+            //else if (number <= 0)
+            //{
+            //    return 0;
+            //}
+            else
+            {
+                //fibonacci = Fibonacci(number - 1) + Fibonacci(number - 2);
+                for (int i = 1; i < number; i++)
+                {
+                    fibonacci = firstNum + secondNum;
+                    firstNum = secondNum;
+                    secondNum = fibonacci;
+                }
+            }
+            return fibonacci;
+
+
+        }
+    }
+
+}
---
+++ Part5/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using Task;
+
+namespace task3
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            LinqToXML linq = new LinqToXML();
+            List<string> list=linq.Linq009();
+            foreach (var l in list)
+                Console.WriteLine(l);
+            Console.ReadLine();
+        }
+    }
+}
