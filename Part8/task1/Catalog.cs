6605885f19482c7524f9af0c043525240c67ab95
Natallia_Sheibak <Natallia_Sheibak@epam.com>
2016-05-11 12:01:58 +0300

    Added Part 8

---
+++ Part8/task1/Catalog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace task1
+{
+    [XmlRoot("catalog", Namespace = "http://library.by/catalog")]
+    public class Catalog
+    {
+        [XmlAttribute("date",DataType = "date")]
+        public DateTime Date { get; set; }
+        [XmlElement("book")]
+        public List<Book> books;
+    }
+}
---
+++ Part8/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializer = new XmlSerializer(typeof(Catalog));
+            var catalog = serializer.Deserialize(new FileStream("books.xml", FileMode.Open)) as Catalog;
+
+            var stream = new FileStream("ser1.xml", FileMode.Create);
+            serializer.Serialize(stream, catalog);
+            stream.Close();
+        }
+    }
+}
