f4db4253de523e292fbbf69df5ac0187bb1e2228
Natallia_Sheibak <Natallia_Sheibak@epam.com>
2016-05-11 12:40:36 +0300

    "Updated Part 8"

---
+++ Part8/Part8/task1/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace task1
+{
+    public enum Genre
+    {
+        [XmlEnum]
+        Computer,
+        [XmlEnum]
+        Fantasy,
+        [XmlEnum]
+        Romance,
+        [XmlEnum]
+        Horror,
+        [XmlEnum("Science Fiction")]
+        ScienceFiction
+    }
+
+    public class Book
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+        [XmlElement("isbn")]
+        public string Isbn { get; set; }
+        [XmlElement("author")]
+        public string Author { get; set; }
+        [XmlElement("title")]
+        public string Title { get; set; }
+        [XmlElement("genre")]
+        public Genre Genre { get; set; }
+        [XmlElement("publisher")]
+        public string Publisher { get; set; }
+        [XmlElement("publish_date", DataType="date")]
+        public DateTime PublishDate { get; set; }
+        [XmlElement("description")]
+        public string Description { get; set; }
+        [XmlElement("registration_date", DataType = "date")]
+        public DateTime RegistrationDate { get; set; }
+    }
+}
---
+++ Part8/Part8/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = "books.xml";
+            string outputFile = "book_ser.xml";
+            Console.WriteLine("Deserialization is in progress...");
+            var serializer = new XmlSerializer(typeof(Catalog));
+            var catalog = serializer.Deserialize(new FileStream(inputFile, FileMode.Open)) as Catalog;
+            if (catalog != null)
+            {
+                Console.WriteLine($"Deserialization of the file {inputFile} is successful\n"+
+                                    "Serialization is in progress...");
+                var stream = new FileStream(outputFile, FileMode.Create);
+                serializer.Serialize(stream, catalog);
+                Console.WriteLine("Serialization is complete");
+                stream.Close();
+            }
+            else
+            {
+                Console.WriteLine($"Deserialization wasn't successful. Check your file for errors");
+            }
+
+
+        }
+    }
+}
